fc660e4d7d263dd38ef7949800f8dcc1f6ce62cd
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2026-08-15 19:51:59 +0330

    create model custom User

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+
+    first_name = models.CharField(max_length=100)
+
+    REQUIRED_FIELDS = []
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.email
