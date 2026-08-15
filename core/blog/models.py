6104e0eeabd5234929ab34712c163fa58fe74f53
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2026-08-15 17:46:09 +0330

    create calss model category and post in the blog app

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+# class post
+class Post(models.Model):
+    auther = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True)
+    status = models.BooleanField(default=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
+# class category
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self) -> str:
+
+        return f" category : '{self.name}'"
